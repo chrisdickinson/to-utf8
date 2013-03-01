2ac60033066b81140b32a2fd3262e589feb72ea8
Chris Dickinson <christopher.s.dickinson@gmail.com>
2013-02-28 17:30:57 -0800

    make it convert to uint8array if buf[idx] does not work

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var test = require('tape')
   , toUTF8 = require('./index')
   , Buffer = require('buffer').Buffer
+  , convert = require('convert-buffer')
 
 test('works as expected', function(assert) {
   var input = ['11000010', '10100010'].map(function(x) { return parseInt(x, 2) })
@@ -10,6 +11,14 @@ test('works as expected', function(assert) {
 
 test('works the same as buffer.toString(utf8)', function(assert) {
   var buf = new Buffer('淾淾淾 hello world 淾淾 yep ƒuu 淾', 'utf8')
-  assert.equal(toUTF8(buf), buf.toString('utf8'))
+    , str = buf.toString('utf8')
+
+  if(buf[0] === undefined) {
+    convert(Uint8Array, function(x, i) { return x.get(i) })
+      .on('data', function(x) { buf = x })
+      .write(buf)
+  }
+
+  assert.equal(toUTF8(buf), str)
   assert.end()
 })
